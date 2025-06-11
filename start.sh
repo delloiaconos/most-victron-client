3ba5378da156ec45fb21614b984ff0765096bfbd
Salvatore Dello Iacono <salvatore.delloiaconos@unibs.it>
2025-06-11 14:32:31 +0200

    Removed log file from start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to start the application in a Docker container.
-python /app/main.py > /app/logs/output.log 2>&1 
+python /app/main.py #> /app/logs/output.log 2>&1 
