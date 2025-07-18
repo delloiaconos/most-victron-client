3e85456245760dbaa5961d6ca661f53173128047
Salvatore Dello Iacono <salvatore.delloiacono@unibs.it>
2025-07-18 13:18:43 +0200

    Changes in exception management.

--- app/thdKeepAlive.py
+++ app/thdKeepAlive.py
@@ -7,7 +7,7 @@ import json
 
 CONSECUTIVE_SUCCESS_TH  = 1
 CONSECUTIVE_FAILS_TH    = 5
-DELTA_KEEPALIVE_SLEEP   = 10
+DELTA_KEEPALIVE_SLEEP   = 45
 
 
 class thdKeepAlive(threading.Thread):
--- app/thdSender.py
+++ app/thdSender.py
@@ -5,13 +5,19 @@ import queue
 import time 
 import json
 
+
 class thdSender(threading.Thread):
     def __init__(self, config, shared ):
         super().__init__()
+        
+        import os
+        self.PRINT_MSG = bool( os.environ.get( 'SENDER_RUN_PRINT_MSG', False ) )
+        
 
         import pytz
         self.tz = pytz.timezone("Europe/Rome")
 
+        
         self.config = config
         self.shared = shared
 
@@ -35,53 +41,59 @@ class thdSender(threading.Thread):
             while not self._stop_event.is_set():
                 try:
                     item = self.q.get(timeout=60)        
-                    #print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) received `{item}`" )
-
-                    try:
-                        topic = item['topic'].split('/')
-                        if "keepalive" in topic:
-                            print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) received `{item}`" )
-
-                        if len( topic ) < 3:
-                            print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) received `{item}`" )
-                            continue
-
-                        if "Hystory" in topic:
-                            continue
-                        
-                        msg = json.loads( item['msg'] )
-
-                        try:
-                            if str( msg['value'] ).lower() in ['none', 'null', 'empty']:
-                                continue
-                        except:
-                            pass
-                        
-                        try:
-                            value = float( msg['value'] )
-                        except:
-                            value = str( msg['value'] )
-                        
-                        try:
-                            bus_id = int( topic[3] )
-                        except:
-                            bus_id = str( topic[3] ) 
-                        
-                        data_point = {
-                            'time'          : item['time'].isoformat(),
-                            'measurement'   : topic[2],
-                            'tags'          : { 'portal_id' : topic[1],
-                                                'site_id' : self.id_site,
-                                                'installation' : self.installation,
-                                                'bus_id' : bus_id },
-                            'fields'        : { "-".join( topic[4:] ) : value },
-                        }
-                        self.client.write_points( [data_point] )
-                    except:
-                        print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) EXCEPTION `{item}`!!" )
+                    if self.PRINT_MSG:
+                        print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) received `{item}`" )
 
                 except queue.Empty:
-                    print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) Queue empty" )    
+                    print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) Queue empty" ) 
+                    continue
+                
+                try:
+                    topic = item['topic'].split('/')
+                except:
+                    continue
+                
+                if "keepalive" in topic:
+                    print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) 'keepalive' `{item}`" )
+
+                if len( topic ) < 4:
+                    print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) 'len( topic ) < 3' on `{item}`" )
+                    continue
+
+                if "Hystory" in topic:
+                    continue
+                
+                msg = json.loads( item['msg'] )
+
+                try:
+                    if str( msg['value'] ).lower() in ['none', 'null', 'empty']:
+                        continue
+                except:
+                    pass
+                
+                try:
+                    value = float( msg['value'] )
+                except:
+                    value = str( msg['value'] )
+                
+                try:
+                    bus_id = int( topic[3] )
+                except:
+                    bus_id = str( topic[3] ) 
+                
+                try:    
+                    data_point = {
+                        'time'          : item['time'].isoformat(),
+                        'measurement'   : topic[2],
+                        'tags'          : { 'portal_id' : topic[1],
+                                            'site_id' : self.id_site,
+                                            'installation' : self.installation,
+                                            'bus_id' : bus_id },
+                        'fields'        : { "-".join( topic[4:] ) : value },
+                    }
+                    self.client.write_points( [data_point] )
+                except:
+                    print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) WRITE EXCEPTION `{data_point}`!!" )
         finally:
             print( f"[SENDER-RUN] ({datetime.now(tz=self.tz)}) Sender thread stopped")
 
